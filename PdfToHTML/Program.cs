c07b550ac3a73c81181424b93c6ac4a381210007
Dhiren Patel <Dhiren Patel>
2018-01-08 23:35:03 +0530

    Add project files.

---
+++ PdfToHTML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PdfToHTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Pdf_To_HTML/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdf_To_HTML
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #region Property
+            //Put your pdf file path
+            string pathToPdf = @"C:/Users/dhire/Downloads/Compressed/Convert PDF file to HTML file in C# - Step by Step/table.pdf";
+            string pathToHtml = Path.ChangeExtension(pathToPdf, ".htm");
+
+            SautinSoft.PdfFocus f = new SautinSoft.PdfFocus();
+            #endregion
+
+            #region Declaration
+            f.HtmlOptions.IncludeImageInHtml = true;
+            f.HtmlOptions.Title = "Simple text";
+
+            f.OpenPdf(pathToPdf);
+
+            if (f.PageCount > 0)
+            {
+                int result = f.ToHtml(pathToHtml);
+
+                if (result == 0)
+                {
+                    System.Diagnostics.Process.Start(pathToHtml);
+                }
+            }
+
+            #endregion
+        }
+    }
+}
